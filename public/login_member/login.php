2466d30dd762a174c5ed32784b773a4da12bcf30
ozyexplorer <ozisambadha@gmail.com>
2017-10-23 11:14:29 +0700

    update data

--- public/login_member/login.php
+++ public/login_member/login.php
@@ -51,7 +51,7 @@
       </style>
       <div class="form style-#{style}" style="width: #{width}">
         <header class="header">
-          <h1 class="active">Sign In</h1>
+          <h1 class="active">LOGIN</h1>
           <h1>Sign Up</h1>
         </header>
         <form class="active">
@@ -116,7 +116,7 @@
       </style>
       <div class="form style-#{style}" style="width: #{width}">
         <header class="header">
-          <h1 class="active">Sign In</h1>
+          <h1 class="active">SIGN UP</h1>
           <h1>Sign Up</h1>
         </header>
         <form class="active">
@@ -150,53 +150,9 @@
           </div>
           <button class="button">Login</button>
         </form>
-        <footer class="footer"><a href="#">Forgot your password?</a></footer>
       </div>
-      <p>Not registered? <a class="toggle" href="#">Sign up</a></p>
     </div>
   </div>
 </div>
-<!-- Tip-->
-<div id="tip">
-  <h1>How to create a form?</h1>
-  <p>It's really simple... If you have basic knowledge of Jade. Simple fill out the paramaters in the mixin below, drop it below this box, and watch as magic happens!</p><code>
-    <h1>Jade</h1>
-    <p> 
-      Please note that these are case sensitive. <br>
-      /* Mixin Guide<br>
-      * Form Container Mixin<br>
-      * +form-container<br>
-      *<br>
-      * Form Mixin<br>
-      * +form('name', 'width', 'color', 'style')<br>
-      *<br>
-      * Input Mixin<br>
-      * +input('type', 'name', 'placeholder', 'label')<br>
-      *<br>
-      * Button Mixin<br>
-      * +button('label')<br>
-      */<br><br>
-    </p>
-    <p><span>+form-container</span></p>
-    <p><span>+form</span><span>.active</span>(<span>'name'</span>, <span>'width'</span>, <span>'color'</span>, <span>'style'</span>)</p>
-    <p><span>+input</span>(<span>'type'</span>, <span>'name'</span>, <span>'placeholder'</span>, <span>'label'</span>)</p>
-    <p><span>+input</span>(<span>'type'</span>, <span>'name'</span>, <span>'placeholder'</span>, <span>'label'</span>)</p>
-    <p><span>+button</span>(<span>'label'</span>)</p>
-    <p><span>+form</span>(<span>'name'</span>, <span>'width'</span>, <span>'color'</span>, <span>'style'</span>)</p>
-    <p><span>+input</span>(<span>'type'</span>, <span>'name'</span>, <span>'placeholder'</span>, <span>'label'</span>)</p>
-    <p><span>+input</span>(<span>'type'</span>, <span>'name'</span>, <span>'placeholder'</span>, <span>'label'</span>)</p>
-    <p><span>+button</span>(<span>'label'</span>)</p>
-    <P><span>p</span> Not registered? </P>
-    <P><span>a</span><span>.toggle</span>(<span>href</span>='#') Sign up</P></code>
-  <h1>Browser Support?</h1>
-  <p>Tested on the latest versions of <b>Chrome</b>, <b>Firefox</b>, <b>Safari</b>, <b>Internet Explorer</b>, &amp; <b>Opera</b>.</p>
-  <p>Best viewed in Chrome because... I said so.</p>
-</div>
-<!-- Portfolio--><a id="portfolio" href="http://andytran.me/" title="View my portfolio!"><i class="fa fa-link"></i></a>
-<!-- CodePen--><a id="codepen" href="https://codepen.io/andytran/" title="Follow me!"><i class="fa fa-codepen"></i></a>
-<!-- Scroll To Bottom--><a id="scrollToBottom" href="#tip" title="Check out the tutorial!"><i class="material-icons">&#xE313;</i></a>
-</div>
-
-	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
