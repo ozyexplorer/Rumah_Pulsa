00f1a31b5afc35d428c868a253de084694285d41
ozyexplorer <ozisambadha@gmail.com>
2017-10-25 13:35:06 +0700

    tambah navbar

--- masuk.php
+++ masuk.php
@@ -15,17 +15,28 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<?php
+	<?php 
 		include 'public/cek_saldo/ceksaldo.php';
-
-		echo 'saldo anda : Rp. '.$data;
-		echo "<br>"; 
-		require_once 'public/daftar_harga/daftar_harga.php';
-
-			
 	?>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Transaksi</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Saldo anda : <?php echo $data ?></a></li>
+		</ul>
+	</nav>
+
+	<?php
+		
+		require_once 'public/daftar_harga/daftar_harga.php';		
+	?>
+
+	
+
 </body>
 </html>
 	
